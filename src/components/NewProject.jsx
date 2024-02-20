2adb15d32000f3c6173327692c0308e94978cb9c
Dorian Kita <dorian.kita90@gmail.com>
2024-02-20 13:53:59 +0100

    added Save and Cancel buttons to input Component and fixed fonts

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,9 +1,19 @@
 import Input from "./Input.jsx";
+import Button from "./Button.jsx";
 
 export default function NewProject() {
   return (
-    <div className="px-8 py-16 w-2/3">
-      <h2 className="text-2xl text-stone-500 pb-16">Create new project.</h2>
+    <div className="px-8 py-16 w-2/6">
+      <div className="flex justify-between items-start">
+        <h2 className="text-2xl text-stone-500 pb-16">Create new project.</h2>
+        <div className="flex w-40 mx-4">
+          <button className="mx-2 text-stone-400 hover:text-stone-900">
+            Cancel
+          </button>
+          <Button>Save</Button>
+        </div>
+      </div>
+
       <Input type="text" label="Title" />
       <Input label="Description" textarea />
       <Input type="date" label="Due Date" />
